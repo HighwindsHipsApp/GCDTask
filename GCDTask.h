7de195cdfb896aa25efa2be39694408ba89a1206
Kevin Hallmark <kevin.hallmark@highwinds.com>
2016-06-20 15:49:52 -0400

    Added project file. Updated GCDTask interfaces to add a designated initializer that takes launch path and arguments. Updated to proper naming conventions and coding standards. Added data hiding and encapsulation. Added .gitignore for the .idea directory.

--- GCDTask.h
+++ GCDTask.h
@@ -7,39 +7,19 @@
 
 #import <Foundation/Foundation.h>
 
-#ifdef GCDTASK_DEBUG
-#define GCDDebug(str, ...) NSLog(str, ##__VA_ARGS__)
-#else
-#define GCDDebug(str, ...)
-#endif
-
-
 @interface GCDTask : NSObject
-{
-    NSPipe* stdoutPipe;
-    NSPipe* stderrPipe;
-    NSPipe* stdinPipe;
-    NSTask* executingTask;
-    id stdoutObserver;
-    id stderrObserver;
-}
 
-@property (strong) NSString* launchPath;
-@property (strong) NSArray* arguments;
-@property BOOL hasExecuted;
-@property __block dispatch_source_t stdoutSource;
-@property __block dispatch_source_t stderrSource;
+- (id)initWithLaunchPath:(NSString *)launchPath andArguments:(NSArray *)arguments;
 
+- (void)launchWithOutputBlock:(void (^)(NSData *))outputHandler
+               andErrorBlock:(void (^)(NSData *))errorHandler
+                    onLaunch:(void (^)())launchHandler
+                      onExit:(void (^)())exitHandler;
 
+- (BOOL)writeStringToStandardInput:(NSString *)input;
+- (BOOL)writeDataToStandardInput:(NSData *)input;
 
-- (void) launchWithOutputBlock: (void (^)(NSData* stdOutData)) stdOut
-                andErrorBlock: (void (^)(NSData* stdErrData)) stdErr
-                     onLaunch: (void (^)()) launched
-                       onExit: (void (^)()) exit;
-
-- (BOOL) WriteStringToStandardInput: (NSString*) input;
-- (BOOL) WriteDataToStandardInput: (NSData*) input;
-- (void) AddArgument: (NSString*) argument;
-- (void) RequestTermination;
+- (void)requestTermination;
+- (void)waitUntilExit;
 
 @end
--- GCDTask.m
+++ GCDTask.m
@@ -6,54 +6,82 @@
 //
 
 #import "GCDTask.h"
+
+
+#ifdef GCDTASK_DEBUG
+#define GCDDebug(str, ...) NSLog(str, ##__VA_ARGS__)
+#else
+#define GCDDebug(str, ...)
+#endif
+
 #define GCDTASK_BUFFER_MAX 4096
+
+@interface GCDTask ()
+
+@property (strong) NSPipe *stdoutPipe;
+@property (strong) NSPipe *stdinPipe;
+@property (strong) NSPipe *stderrPipe;
+
+@property (strong) NSTask *executingTask;
+
+@property (strong) NSString* launchPath;
+@property (strong) NSArray* arguments;
+
+@property BOOL hasExecuted;
+
+@property __block dispatch_source_t stdoutSource;
+@property __block dispatch_source_t stderrSource;
+
+@end
 
 @implementation GCDTask
 
-- (id) init
-{
-    return [super init];
+- (id)initWithLaunchPath:(NSString *)launchPath andArguments:(NSArray *)arguments {
+    self = [super init];
+
+    if (self) {
+        _launchPath = launchPath;
+        _arguments = arguments;
+    }
+
+    return self;
 }
 
-- (void) launchWithOutputBlock: (void (^)(NSData* stdOutData)) stdOut
-                 andErrorBlock: (void (^)(NSData* stdErrData)) stdErr
-                      onLaunch: (void (^)()) launched
-                        onExit: (void (^)()) exit
-{
-    executingTask = [[NSTask alloc] init];
+- (void)launchWithOutputBlock:(void (^)(NSData *))outputHandler
+                andErrorBlock:(void (^)(NSData *))errorHandler
+                     onLaunch:(void (^)())launchHandler
+                       onExit:(void (^)())exitHandler {
+
+    // Setup a local variable
+    NSTask *executingTask = [[NSTask alloc] init];
  
     /* Set launch path. */
     [executingTask setLaunchPath:[_launchPath stringByStandardizingPath]];
     
-    if (![[NSFileManager defaultManager] isExecutableFileAtPath:[executingTask launchPath]])
-    {
+    if (![[NSFileManager defaultManager] isExecutableFileAtPath:[executingTask launchPath]]) {
         @throw [NSException exceptionWithName:@"GCDTASK_INVALID_EXECUTABLE" reason:@"There is no executable at the path set." userInfo:nil];
     }
 
     /* Clean then set arguments. */
-    for (id arg in _arguments)
-    {
-        if([arg class] != [NSString class])
-        {
+    for (id arg in _arguments) {
+        if([arg class] != [NSString class]) {
             NSMutableArray* cleanedArray = [[NSMutableArray alloc] init];
             /* Clean up required! */
-            for (id arg in _arguments)
-            {
+            for (id arg in _arguments) {
                 [cleanedArray addObject:[NSString stringWithFormat:@"%@",arg]];
             }
+
             [self setArguments:cleanedArray];
             break;
         }
     }
     
-    
     [executingTask setArguments:_arguments];
-    
-    
+
     /* Setup pipes */
-    stdinPipe = [NSPipe pipe];
-    stdoutPipe = [NSPipe pipe];
-    stderrPipe = [NSPipe pipe];
+    NSPipe *stdinPipe = [NSPipe pipe];
+    NSPipe *stdoutPipe = [NSPipe pipe];
+    NSPipe *stderrPipe = [NSPipe pipe];
     
     [executingTask setStandardInput:stdinPipe];
     [executingTask setStandardOutput:stdoutPipe];
@@ -63,50 +91,58 @@
     /* TODO: Potentially make this changeable? Surely there's probably a nicer way to get the CWD too. */
     [executingTask setCurrentDirectoryPath:[[[NSFileManager alloc] init] currentDirectoryPath]];
 
-    
+
     /* Ensure the pipes are non-blocking so GCD can read them correctly. */
     fcntl([stdoutPipe fileHandleForReading].fileDescriptor, F_SETFL, O_NONBLOCK);
     fcntl([stderrPipe fileHandleForReading].fileDescriptor, F_SETFL, O_NONBLOCK);
     
     /* Setup a dispatch source for both descriptors. */
-    _stdoutSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ,[stdoutPipe fileHandleForReading].fileDescriptor, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
-    _stderrSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ,[stderrPipe fileHandleForReading].fileDescriptor, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+    self.stdoutSource = dispatch_source_create(
+        DISPATCH_SOURCE_TYPE_READ,
+        [stdoutPipe fileHandleForReading].fileDescriptor,
+        0,
+        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    );
+
+    self.stderrSource = dispatch_source_create(
+        DISPATCH_SOURCE_TYPE_READ,
+        [stderrPipe fileHandleForReading].fileDescriptor,
+        0,
+        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    );
     
     /* Set stdout source event handler to read data and send it out. */
-    dispatch_source_set_event_handler(_stdoutSource, ^ {
+    dispatch_source_set_event_handler(self.stdoutSource, ^ {
         void* buffer = malloc(GCDTASK_BUFFER_MAX);
         ssize_t bytesRead;
         
-        do
-        {
+        do {
             errno = 0;
             bytesRead = read([stdoutPipe fileHandleForReading].fileDescriptor, buffer, GCDTASK_BUFFER_MAX);
         } while(bytesRead == -1 && errno == EINTR);
         
-        if(bytesRead > 0)
-        {
+        if(bytesRead > 0) {
             // Create before dispatch to prevent a race condition.
             NSData* dataToPass = [NSData dataWithBytes:buffer length:bytesRead];
             dispatch_async(dispatch_get_main_queue(), ^{
-                if(!_hasExecuted)
-                {
-                    if(launched)
-                        launched();
+                if(!_hasExecuted) {
+                    if(launchHandler) {
+                        launchHandler();
+                    }
                     _hasExecuted = TRUE;
                 }
-                if(stdOut)
-                {
-                    stdOut(dataToPass);
+                if(outputHandler) {
+                    outputHandler(dataToPass);
                 }
             });
         }
         
-        if(errno != 0 && bytesRead <= 0)
-        {
-            dispatch_source_cancel(_stdoutSource);
+        if(errno != 0 && bytesRead <= 0) {
+            dispatch_source_cancel(self.stdoutSource);
             dispatch_async(dispatch_get_main_queue(), ^{
-                if(exit)
-                    exit();
+                if(exitHandler) {
+                    exitHandler();
+                }
             });
         }
 
@@ -115,86 +151,95 @@
     });
     
     /* Same thing for stderr. */
-    dispatch_source_set_event_handler(_stderrSource, ^ {
+    dispatch_source_set_event_handler(self.stderrSource, ^ {
         void* buffer = malloc(GCDTASK_BUFFER_MAX);
         ssize_t bytesRead;
         
-        do
-        {
+        do {
             errno = 0;
             bytesRead = read([stderrPipe fileHandleForReading].fileDescriptor, buffer, GCDTASK_BUFFER_MAX);
         } while(bytesRead == -1 && errno == EINTR);
         
-        if(bytesRead > 0)
-        {
+        if(bytesRead > 0) {
             NSData* dataToPass = [NSData dataWithBytes:buffer length:bytesRead];
             dispatch_async(dispatch_get_main_queue(), ^{
-                if(stdErr)
-                {
-                    stdErr(dataToPass);
+                if(errorHandler) {
+                    errorHandler(dataToPass);
                 }
             });
         }
         
-        if(errno != 0 && bytesRead <= 0)
-        {
-            dispatch_source_cancel(_stderrSource);
+        if(errno != 0 && bytesRead <= 0) {
+            dispatch_source_cancel(self.stderrSource);
         }
         
         free(buffer);
     });
 
     
-    dispatch_resume(_stdoutSource);
-    dispatch_resume(_stderrSource);
+    dispatch_resume(self.stdoutSource);
+    dispatch_resume(self.stderrSource);
 
-    executingTask.terminationHandler = ^(NSTask* task)
-    {
-        dispatch_source_cancel(_stdoutSource);
-        dispatch_source_cancel(_stderrSource);
-        if(exit)
-            exit();
+    executingTask.terminationHandler = ^(NSTask* task) {
+        dispatch_source_cancel(self.stdoutSource);
+        dispatch_source_cancel(self.stderrSource);
+
+        if(exitHandler) {
+            exitHandler();
+        }
     };
 
-    [executingTask launch];
+    // Set all our local variables to the class variables
+    self.stdinPipe = stdinPipe;
+    self.stdoutPipe = stdoutPipe;
+    self.stderrPipe = stderrPipe;
+    self.executingTask = executingTask;
+
+    [self.executingTask launch];
 }
 
-- (BOOL) WriteStringToStandardInput: (NSString*) input
-{
-    return [self WriteDataToStandardInput:[input dataUsingEncoding:NSUTF8StringEncoding]];
+/**
+ * Converts the input to an NSData and calls writeDataToStandardInput.
+ */
+-(BOOL)writeStringToStandardInput:(NSString *)input {
+    return [self writeDataToStandardInput:[input dataUsingEncoding:NSUTF8StringEncoding]];
 }
-
 
 /* Currently synchronous. TODO: Async fun! */
-- (BOOL) WriteDataToStandardInput: (NSData*) input
-{
-    if (!stdinPipe || stdinPipe == nil)
-    {
+
+-(BOOL)writeDataToStandardInput:(NSData *)input {
+    if (!self.stdinPipe || self.stdinPipe == nil) {
         GCDDebug(@"Standard input pipe does not exist.");
         return NO;
     }
     
-    [[stdinPipe fileHandleForWriting] writeData:input];
+    [[self.stdinPipe fileHandleForWriting] writeData:input];
+
     return YES;
 }
 
-/* If you don't like setting your own array. You really should never have a use for this. */
-- (void) AddArgument: (NSString*) argument
-{
-    NSMutableArray* temp = [NSMutableArray arrayWithArray:_arguments];
-    [temp addObject:argument];
-    [self setArguments:temp];
+/**
+ * Terminates the GCDTask. First it will request SIGINT, then it will force SIGTERM after 10 seconds.
+ */
+- (void)requestTermination {
+    [self.executingTask interrupt];
+
+    dispatch_after(
+        dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)),
+        dispatch_get_main_queue(),
+        ^(void) {
+            [self.executingTask terminate];
+        }
+    );
 }
 
-- (void) RequestTermination
-{
-    /* Ask nicely for SIGINT, then SIGTERM. */
-    [executingTask interrupt];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void)
-    {
-        [executingTask terminate];
-    });
+/**
+ * Calls waitUntilExit on the currently executing internal NSTask.
+ */
+- (void)waitUntilExit {
+    if (self.executingTask) {
+        [self.executingTask waitUntilExit];
+    }
 }
-
 
 @end
